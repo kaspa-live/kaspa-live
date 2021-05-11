256a3bb14155fb012ddf5ef2a09720cf8376735a
stasatdaglabs <stas@daglabs.com>
2021-05-11 16:11:41 +0300

    Add types to generated files.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import {AppProps} from 'next/app'
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />
 }
 
 export default App
