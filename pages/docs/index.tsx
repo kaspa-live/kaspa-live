89f5903ce470ece4bb9d12de36e2bf38a6543d7c
stasatdaglabs <stas@daglabs.com>
2021-06-08 17:18:25 +0300

    Add a bunch more questions.

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -7,6 +7,13 @@ const Docs = () => {
             <div className={styles.navigation}>
                 <ul>
                     <li><a href="#what-is-kaspa">What is Kaspa?</a></li>
+                    <li><a href="#how-is-kaspa-different-from-other-cryptocurrencies">How is Kaspa different from other cryptocurrencies?</a></li>
+                    <li><a href="#whats-the-current-status-of-the-project">What's the current status of the project?</a></li>
+                    <li><a href="#how-can-i-run-a-kaspa-node">How can I run a Kaspa node?</a></li>
+                    <li><a href="#how-can-i-contribute">How can I contribute?</a></li>
+                    <li><a href="#will-there-be-pre-mining">Will there be any pre-mining? What about founder's rewards?</a></li>
+                    <li><a href="#will-testnet-coins-be-transferred-over-to-mainnet">Will Testnet coins be transferred over to Mainnet?</a></li>
+                    <li><a href="#when-will-mainnet-be-launched">When will Mainnet be launched?</a></li>
                     <li><a href="#how-does-kaspa-work">How does Kaspa work?</a></li>
                 </ul>
             </div>
@@ -14,85 +21,55 @@ const Docs = () => {
                 <a id="what-is-kaspa" className={styles.anchor}/>
                 <div>
                     <h1>What is Kaspa?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod non nunc vitae
-                        convallis. Sed quis ex quis neque dignissim fringilla nec dignissim sapien. Praesent volutpat
-                        laoreet odio nec varius. Nulla at iaculis velit. Phasellus in varius dui. Maecenas accumsan
-                        massa erat, a viverra elit aliquet nec. Nullam sit amet eros leo. Vivamus ut venenatis dolor.
-                        Integer suscipit et diam ac pulvinar. Aenean sed elementum nunc. Sed sollicitudin odio ut
-                        tincidunt iaculis. Mauris tempor enim id bibendum aliquam. Nullam posuere bibendum porttitor.
-                        Quisque et turpis elementum, dapibus orci id, pellentesque odio.</p>
+                    <p>Kaspa is...</p>
+                </div>
 
-                    <p> Sed convallis nec libero id iaculis. Maecenas accumsan lorem massa, ut auctor arcu venenatis
-                        non. Duis vel nulla a sem lacinia condimentum. Nullam sollicitudin vehicula nulla quis congue.
-                        Maecenas ac massa suscipit mi laoreet dapibus. Donec vel volutpat arcu. Etiam varius dui nec
-                        arcu consequat, vel tempor metus dapibus. Interdum et malesuada fames ac ante ipsum primis in
-                        faucibus. Maecenas vitae odio elit. Proin sagittis orci lacus, at consequat ante semper in. Sed
-                        nec dignissim tellus, sit amet aliquam tellus. Nulla lorem elit, porttitor a justo a, consequat
-                        accumsan dui. Suspendisse a orci vel tellus sagittis laoreet.</p>
+                <a id="how-is-kaspa-different-from-other-cryptocurrencies" className={styles.anchor}/>
+                <div>
+                    <h1>How is Kaspa different from other cryptocurrencies?</h1>
+                    <p>Kaspa is different because...</p>
+                </div>
 
-                    <p> Proin laoreet nunc varius, dictum ligula ac, venenatis odio. Integer ut aliquam nisl, nec
-                        facilisis sapien. Donec volutpat id lectus eget interdum. Phasellus lobortis, lectus non
-                        condimentum mollis, lorem lacus rhoncus felis, in tincidunt mauris lacus vel augue. Praesent
-                        vitae dui gravida, blandit mi nec, vulputate lorem. Cras finibus vestibulum arcu, eu vestibulum
-                        magna dignissim vitae. In hac habitasse platea dictumst.</p>
+                <a id="whats-the-current-status-of-the-project" className={styles.anchor}/>
+                <div>
+                    <h1>What's the current status of the project?</h1>
+                    <p>The current status of the project is...</p>
+                </div>
 
-                    <p> Mauris et tempus lorem, ullamcorper consectetur quam. Donec eros ante, ullamcorper in elit quis,
-                        molestie placerat erat. Phasellus sit amet condimentum nibh. Praesent luctus lectus id nisl
-                        consequat semper. Nullam ac libero sed risus commodo blandit. Aenean vitae fringilla lacus, in
-                        consequat purus. Quisque ut pulvinar nulla, in sagittis diam. Integer et ligula eu odio suscipit
-                        pulvinar. Morbi massa sapien, varius ut nisl at, rutrum placerat magna. Nunc malesuada urna in
-                        molestie semper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin nec erat
-                        eget ante ornare elementum. Aliquam erat volutpat. Quisque faucibus neque ante, nec volutpat
-                        justo fermentum et.</p>
+                <a id="how-can-i-run-a-kaspa-node" className={styles.anchor}/>
+                <div>
+                    <h1>How can I run a Kaspa node?</h1>
+                    <p>A Kaspa node can be run by...</p>
+                </div>
 
-                    <p> Nullam interdum lectus vel tellus sollicitudin, non laoreet tortor tincidunt. Nam convallis odio
-                        justo, quis iaculis purus pharetra quis. Donec eleifend porta quam quis sagittis. Sed posuere
-                        magna lorem, ac malesuada quam venenatis ut. Quisque eu ligula non ligula placerat ultricies at
-                        vitae eros. Donec sagittis elit sed ante congue, at placerat felis laoreet. Morbi mattis odio in
-                        ex semper tincidunt at sit amet tortor. Morbi malesuada nisi nec nulla sollicitudin tincidunt.
-                        Curabitur bibendum eros nec tellus tempus varius. Quisque vestibulum in est ac bibendum. </p>
+                <a id="how-can-i-contribute" className={styles.anchor}/>
+                <div>
+                    <h1>How can I contribute?</h1>
+                    <p>You can contribute by...</p>
+                </div>
+
+                <a id="will-there-be-pre-mining" className={styles.anchor}/>
+                <div>
+                    <h1>Will there be any pre-mining? What about founder's rewards?</h1>
+                    <p>There will be neither pre-mining nor founder's rewards.</p>
+                </div>
+
+                <a id="will-testnet-coins-be-transferred-over-to-mainnet" className={styles.anchor}/>
+                <div>
+                    <h1>Will Testnet coins be transferred over to Mainnet?</h1>
+                    <p>Testnet means X, Mainnet means Y.</p>
+                </div>
+
+                <a id="when-will-mainnet-be-launched" className={styles.anchor}/>
+                <div>
+                    <h1>When will Mainnet be launched?</h1>
+                    <p>Mainnet will be launched at...</p>
                 </div>
 
                 <a id="how-does-kaspa-work" className={styles.anchor}/>
                 <div>
                     <h1>How does Kaspa work?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod non nunc vitae
-                        convallis. Sed quis ex quis neque dignissim fringilla nec dignissim sapien. Praesent volutpat
-                        laoreet odio nec varius. Nulla at iaculis velit. Phasellus in varius dui. Maecenas accumsan
-                        massa erat, a viverra elit aliquet nec. Nullam sit amet eros leo. Vivamus ut venenatis dolor.
-                        Integer suscipit et diam ac pulvinar. Aenean sed elementum nunc. Sed sollicitudin odio ut
-                        tincidunt iaculis. Mauris tempor enim id bibendum aliquam. Nullam posuere bibendum porttitor.
-                        Quisque et turpis elementum, dapibus orci id, pellentesque odio.</p>
-
-                    <p> Sed convallis nec libero id iaculis. Maecenas accumsan lorem massa, ut auctor arcu venenatis
-                        non. Duis vel nulla a sem lacinia condimentum. Nullam sollicitudin vehicula nulla quis congue.
-                        Maecenas ac massa suscipit mi laoreet dapibus. Donec vel volutpat arcu. Etiam varius dui nec
-                        arcu consequat, vel tempor metus dapibus. Interdum et malesuada fames ac ante ipsum primis in
-                        faucibus. Maecenas vitae odio elit. Proin sagittis orci lacus, at consequat ante semper in. Sed
-                        nec dignissim tellus, sit amet aliquam tellus. Nulla lorem elit, porttitor a justo a, consequat
-                        accumsan dui. Suspendisse a orci vel tellus sagittis laoreet.</p>
-
-                    <p> Proin laoreet nunc varius, dictum ligula ac, venenatis odio. Integer ut aliquam nisl, nec
-                        facilisis sapien. Donec volutpat id lectus eget interdum. Phasellus lobortis, lectus non
-                        condimentum mollis, lorem lacus rhoncus felis, in tincidunt mauris lacus vel augue. Praesent
-                        vitae dui gravida, blandit mi nec, vulputate lorem. Cras finibus vestibulum arcu, eu vestibulum
-                        magna dignissim vitae. In hac habitasse platea dictumst.</p>
-
-                    <p> Mauris et tempus lorem, ullamcorper consectetur quam. Donec eros ante, ullamcorper in elit quis,
-                        molestie placerat erat. Phasellus sit amet condimentum nibh. Praesent luctus lectus id nisl
-                        consequat semper. Nullam ac libero sed risus commodo blandit. Aenean vitae fringilla lacus, in
-                        consequat purus. Quisque ut pulvinar nulla, in sagittis diam. Integer et ligula eu odio suscipit
-                        pulvinar. Morbi massa sapien, varius ut nisl at, rutrum placerat magna. Nunc malesuada urna in
-                        molestie semper. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin nec erat
-                        eget ante ornare elementum. Aliquam erat volutpat. Quisque faucibus neque ante, nec volutpat
-                        justo fermentum et.</p>
-
-                    <p> Nullam interdum lectus vel tellus sollicitudin, non laoreet tortor tincidunt. Nam convallis odio
-                        justo, quis iaculis purus pharetra quis. Donec eleifend porta quam quis sagittis. Sed posuere
-                        magna lorem, ac malesuada quam venenatis ut. Quisque eu ligula non ligula placerat ultricies at
-                        vitae eros. Donec sagittis elit sed ante congue, at placerat felis laoreet. Morbi mattis odio in
-                        ex semper tincidunt at sit amet tortor. Morbi malesuada nisi nec nulla sollicitudin tincidunt.
-                        Curabitur bibendum eros nec tellus tempus varius. Quisque vestibulum in est ac bibendum. </p>
+                    <p>Kaspa works by...</p>
                 </div>
             </div>
         </div>
