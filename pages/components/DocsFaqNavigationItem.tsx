ae717083b649194f3a46613c2d9820ac82075092
stasatdaglabs <stas@daglabs.com>
2021-06-10 17:07:25 +0300

    Make the FAQ look nicer.

--- pages/components/DocsFaqNavigationItem.tsx
+++
@@ -1,5 +0,0 @@
-const DocsFaqNavigationItem = ({text, anchorName}: { text: string, anchorName: string }) => {
-    return <li><a href={`#${anchorName}`}>{text}</a></li>
-};
-
-export default DocsFaqNavigationItem;
--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,28 +1,23 @@
 import CommonLayout from "../components/CommonLayout";
 import styles from "../../styles/Docs.module.css";
-import DocsFaqNavigationItem from "../components/DocsFaqNavigationItem";
-import DocsFaqItem from "../components/DocsFaqItem";
 import {ReactFragment} from "react";
+import {Accordion, AccordionDetails, AccordionSummary, Typography} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const Docs = () => {
     return <CommonLayout>
         <div className={styles["docs-container"]}>
-            <div className={styles.navigation}>
-                <ul>
-                    {faqItems.map(faqItem =>
-                        <DocsFaqNavigationItem
-                            anchorName={faqItem.anchorName}
-                            text={faqItem.questionText}/>)}
-                </ul>
-            </div>
-            <div className={styles.content}>
-                <h1>FAQ</h1>
-                {faqItems.map(faqItem =>
-                    <DocsFaqItem
-                        anchorName={faqItem.anchorName}
-                        questionText={faqItem.questionText}
-                        answerText={faqItem.answerText}/>)}
-            </div>
+            <Typography variant="h3" className={styles.header}>FAQ</Typography>
+            {faqItems.map(faqItem =>
+                <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                        <Typography variant="h5">{faqItem.questionText}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography variant="body1">{faqItem.answerText}</Typography>
+                    </AccordionDetails>
+                </Accordion>
+            )}
         </div>
     </CommonLayout>
 };
@@ -40,7 +35,7 @@ const faqItems: FaqItem[] = [
     {
         anchorName: "what-is-kaspa",
         questionText: "What is Kaspa? How is Kaspa different from other cryptocurrencies?",
-        answerText: <>Kaspa is a cryptocurrency similar to Bitcoin. It is public, decentralized, and permissionless.
+        answerText: <p>Kaspa is a cryptocurrency similar to Bitcoin. It is public, decentralized, and permissionless.
             Its security model is based on Proof-of-Work.<br/>
             <br/>
             In Bitcoin, increasing transaction throughput negatively impacts its security. This sets an effective limit
@@ -55,52 +50,52 @@ const faqItems: FaqItem[] = [
                 href="https://someone235.medium.com/scaling-bitcoin-with-blockdag-d785174a3402" target="_blank">how DAGs
                 avoid
                 it.</a>
-        </>,
+        </p>,
     },
     {
         anchorName: "how-does-kaspa-work",
         questionText: "How does Kaspa work?",
-        answerText: <>Kaspa is an implementation of the GHOSTDAG protocol, a greedy version of PHANTOM. Both protocols
+        answerText: <p>Kaspa is an implementation of the GHOSTDAG protocol, a greedy version of PHANTOM. Both protocols
             were originally authored by Yonatan Sompolinsky, and may be found <a href="https://eprint.iacr.org/2018/104"
                                                                                  target="_blank">here.</a><br/>
             <br/>
             Ori Newman, a Kaspa core developer, has written
             an <a href="https://someone235.medium.com/scaling-bitcoin-with-blockdag-d785174a3402" target="_blank">in-depth
-                article</a> exploring the PHANTOM protocol.</>,
+                article</a> exploring the PHANTOM protocol.</p>,
     },
     {
         anchorName: "whats-the-current-status-of-the-project",
         questionText: "What's the current status of the project?",
-        answerText: <>Kaspa Testnet has been launched on April 8th, 2021.<br/>
+        answerText: <p>Kaspa Testnet has been launched on April 8th, 2021.<br/>
             The roadmap towards the Mainnet launch may be
-            seen on <a href="https://github.com/kaspanet/kaspad/projects/4" target="_blank">Github.</a></>,
+            seen on <a href="https://github.com/kaspanet/kaspad/projects/4" target="_blank">Github.</a></p>,
     },
     {
         anchorName: "how-can-i-run-a-kaspa-node",
         questionText: "How can I run a Kaspa node?",
-        answerText: <>The most straightforward way to run a Kaspa node is to download
+        answerText: <p>The most straightforward way to run a Kaspa node is to download
             the <a href="https://github.com/kaspanet/kaspad/releases" target="_blank">latest binaries</a> of kaspad, the
             reference implementation of a Kaspa node. Once the binaries have been downloaded for your OS, simply run the
             kaspad executable.
-        </>,
+        </p>,
     },
     {
         anchorName: "how-can-i-contribute",
         questionText: "How can I contribute?",
-        answerText: <>Code contributions may be made to <a href="https://github.com/kaspanet/kaspad/"
-                                                           target="_blank">kaspad,</a> the repository for Kaspa's main
+        answerText: <p>Code contributions may be made to <a href="https://github.com/kaspanet/kaspad/"
+                                                            target="_blank">kaspad,</a> the repository for Kaspa's main
             node.<br/>
             Documentation contributions may be made to <a href="https://github.com/kaspanet/docs/"
                                                           target="_blank">the docs repository.</a><br/>
             For a list of good first issues please go <a
                 href="https://github.com/kaspanet/kaspad/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+first+issue%22"
                 target="_blank">here.</a>
-        </>,
+        </p>,
     },
     {
         anchorName: "will-there-be-pre-mining",
         questionText: "Will there be any pre-mining? What about founder's rewards?",
-        answerText: <>There will be neither pre-mining nor founder's rewards.<br/>
+        answerText: <p>There will be neither pre-mining nor founder's rewards.<br/>
             <br/>
             The original Kaspa researchers and developers are funded by <a href="https://www.daglabs.com/"
                                                                            target="_blank">DAGlabs</a>,
@@ -110,7 +105,7 @@ const faqItems: FaqItem[] = [
             Yonatan Sompolinsky, DAGlabs' founder, has written <a
                 href="https://hashdag.medium.com/in-which-i-love-my-truly-truly-fair-60e74cbaaf7b" target="_blank">an
                 article</a> on the matter.
-        </>,
+        </p>,
     },
     {
         anchorName: "will-testnet-coins-be-transferred-over-to-mainnet",
