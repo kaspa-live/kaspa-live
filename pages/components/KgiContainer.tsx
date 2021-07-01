ab4f8817dd918408f2ce6e22a0f7cbd16a408e24
stasatdaglabs <stas@daglabs.com>
2021-07-01 17:54:51 +0300

    The previous commit turned out to not be supported by next.js.

--- pages/components/KgiContainer.tsx
+++ pages/components/KgiContainer.tsx
@@ -1,12 +1,14 @@
 import {useEffect, useRef} from "react";
 import styles from '../../styles/KgiContainer.module.css'
 import {renderToStaticMarkup} from "react-dom/server";
-import {kgiAddress} from "../addresses";
 
 const KgiContainer = () => {
     const containerRef = useRef() as React.MutableRefObject<HTMLDivElement>;
 
     useEffect(() => {
+        const rawKgiAddress = process.env.NEXT_PUBLIC_KGI_ADDRESS;
+        const kgiAddress = `${window.location.protocol}//${rawKgiAddress}`;
+
         const container = containerRef.current;
         const kgiFrameElement =
             <iframe src={kgiAddress} className={styles['kgi-frame']}
